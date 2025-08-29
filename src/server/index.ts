cbdc98c534aa72e5cf72ffbaa370075ac118a3d0
rkterungwa16 <terungwakombol@gmail.com>
2025-08-29 06:28:05 +0100

    set up application loggin

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,12 @@
 import * as dotenv from "dotenv";
-// import { createAppLogger } from "./middlewares/logger.middleware";
+import { createAppLogger } from "./middlewares/logger.middleware";
 // import { init } from "./server";
 
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 3200;
 dotenv.config();
 // init().listen(port);
-// const applicationStartLog = createAppLogger("Terk Cards for Humanity");
-// applicationStartLog.log({
-//   level: "info",
-//   message: `Application is starting at port ${port}`,
-// });
+const applicationStartLog = createAppLogger("Terk Cards for Humanity");
+applicationStartLog.log({
+  level: "info",
+  message: `Application is starting at port ${port}`,
+});
